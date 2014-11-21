cc6ab5eeba5fd7a819d46231e0bbdd9cbf749d3b
Steven Farthing <sfarthin@gmail.com>
2014-11-21 11:24:38 -0500

    Add formatted table

--- module.js
+++ module.js
@@ -1,16 +1,43 @@
 var googleapis 	= require('googleapis'),
 	OAuth2 		= googleapis.auth.OAuth2,
 	fs			= require("fs"),
+	_ 			= require("lodash"),
 	moment 		= require("moment");
+
+_.mixin({
+  compactObject : function(o) {
+     var clone = _.clone(o);
+     _.each(clone, function(v, k) {
+       if(!v) {
+         delete clone[k];
+       }
+     });
+     return clone;
+  }
+});
 
 module.exports = function(settings, final_callback) {
 	
-	var GOOGLEAPI_CLIENTID = (settings.clientId ? settings.clientId : process.env.GOOGLEAPI_CLIENTID),
-		GOOGLEAPI_EMAIL = (settings.serviceEmail ? settings.serviceEmail : process.env.GOOGLEAPI_EMAIL),
-		GOOGLEAPI_KEY = (settings.key ? settings.key : process.env.GOOGLEAPI_KEY),
-		GOOGLEAPI_ANALYTICS_TABLE = (settings.ids ? settings.ids : process.env.GOOGLEAPI_ANALYTICS_TABLE);
+	try {
+		var GOOGLEAPI_CLIENTID = (settings.clientId ? settings.clientId : process.env.GOOGLEAPI_CLIENTID),
+			GOOGLEAPI_EMAIL = (settings.serviceEmail ? settings.serviceEmail : process.env.GOOGLEAPI_EMAIL),
+			GOOGLEAPI_KEY = (settings.key ? settings.key : process.env.GOOGLEAPI_KEY),
+			GOOGLEAPI_ANALYTICS_TABLE = (settings.ids ? settings.ids : process.env.GOOGLEAPI_ANALYTICS_TABLE);
 
-	var sessionFile = process.env.TMPDIR + "ga-analytics-" + GOOGLEAPI_EMAIL.replace(/[^a-zA-Z\-]/gi, "_");
+		var sessionFile = process.env.TMPDIR + "ga-analytics-" + GOOGLEAPI_EMAIL.replace(/[^a-zA-Z\-]/gi, "_");	
+		
+		if(!GOOGLEAPI_CLIENTID || !GOOGLEAPI_EMAIL || !GOOGLEAPI_KEY || !GOOGLEAPI_ANALYTICS_TABLE) throw "Missing";
+		
+	} catch(e) {
+		var error = "\n A valid client id, service email, key, and table are required" +
+					"\n\n OAuth Client id: " + settings.clientId +
+					"\n Service Email: " + settings.serviceEmail + 
+					"\n Path to Private Key: " + settings.key +
+					"\n Google Analytics view: " + GOOGLEAPI_ANALYTICS_TABLE;
+		
+		final_callback(new Error(error));
+		return;
+	}
 
 	if(!settings.startDate)
 		settings.startDate = moment().subtract(moment.duration(1, 'M')).format('YYYY-MM-DD');
@@ -31,7 +58,12 @@ module.exports = function(settings, final_callback) {
 						return;
 					}
 					
-					var json = JSON.parse(res);
+					try {
+						var json = JSON.parse(res);
+					} catch(e) {
+						jwt.authorize(callback);
+						return;
+					}
 					
 					if(new Date(json.expiry_date) > Date.now()) {
 						callback(null, json);
@@ -63,13 +95,17 @@ module.exports = function(settings, final_callback) {
 	
 		// https://developers.google.com/analytics/devguides/reporting/core/dimsmets
 		// https://developers.google.com/analytics/devguides/reporting/core/v3/coreDevguide
-		googleapis.analytics('v3').data.ga.get({
+		googleapis.analytics('v3').data.ga.get(_.compactObject({
 		    "ids": GOOGLEAPI_ANALYTICS_TABLE,
 		    "start-date": settings.startDate,
 		    "end-date": settings.endDate,
 		    "metrics": settings.metrics,
+			"filters": settings.filter,
+			'dimensions': settings.dimensions,
+			"max-results": settings.max,
+			sort: settings.sort,
 			auth: oauth2Client
-		}, function(err, r) {
+		}), function(err, r) {
 			
 			if(err) {
 				final_callback(err);
